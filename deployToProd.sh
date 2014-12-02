0d351e00cbef5cc441bb2fe7f2bc62fed34043da
Joddsson <ivaroddsson@gmail.com>
2014-12-02 11:44:21 +0000

    Debugging deploy src

--- deployToProd.sh
+++ deployToProd.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 echo "in deploy script"
-
 docker push joddsson/tictactoe
 
 ssh root@178.62.248.172 '
+echo "Successful ssh"
+
 docker kill tictactoe
+echo "After kill"
+
 docker rm tictactoe
+echo "After rm"
+
 docker pull joddsson/tictactoe
+echo "After pull"
+
 docker run -p 80:8080 -d -e "NODE_ENV=production" --name="tictactoe" joddsson/tictactoe
+echo "After run"
+
 exit
 '
 
