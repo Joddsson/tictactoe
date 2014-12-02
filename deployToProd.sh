a6f67240966c30e5285eaf73f2225d121e9e9f6b
Joddsson <ivaroddsson@gmail.com>
2014-12-02 12:05:37 +0000

    Changed ip address from integration to prod server

--- deployToProd.sh
+++ deployToProd.sh
@@ -2,7 +2,7 @@
 echo "in deploy script"
 docker push joddsson/tictactoe
 
-ssh -o StrictHostKeyChecking=no root@178.62.248.172 '
+ssh root@178.62.225.199 '
 echo "Successful ssh"
 
 docker kill tictactoe
