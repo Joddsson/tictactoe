2aaa2a759ea7a5d506f71185e71dc38fea93f4dd
Joddsson <ivaroddsson@gmail.com>
2014-12-07 17:10:23 +0000

    Trying to get response

--- server/api/createGame/index.js
+++ server/api/createGame/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var ctrl	= require('../command.controller.js');
-var router 	= express.Router();
 
-router.post('/', ctrl.executeCommand);
+module.exports = function(app){
+	var router 	= express.Router();
 
-return { router:router }
+	router.post('/', ctrl.executeCommand);
+
+	return { router:router }
+}
